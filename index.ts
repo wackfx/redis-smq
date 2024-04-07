930c0ea40973c45f4b9d322c456be1934648d900
wack <135170502+wackfx@users.noreply.github.com>
2024-04-08 01:18:04 +0200

    export workers

--- index.ts
+++ index.ts
@@ -6,7 +6,11 @@
  * This source code is licensed under the MIT license found in the LICENSE file
  * in the root directory of this source tree.
  */
-
+import PublishScheduledWorker from './src/lib/consumer/workers/publish-scheduled.worker.js';
+import RequeueUnacknowledgedWorker from './src/lib/consumer/workers/requeue-unacknowledged.worker.js';
+import DelayUnacknowledgedWorker from './src/lib/consumer/workers/delay-unacknowledged.worker.js';
+import WatchConsumersWorker from './src/lib/consumer/workers/watch-consumers.worker.js';
+export { PublishScheduledWorker, RequeueUnacknowledgedWorker, DelayUnacknowledgedWorker, WatchConsumersWorker };
 export * from './src/common/index.js';
 export * from './src/config/index.js';
 export * from './src/lib/index.js';
